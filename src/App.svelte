862f1d64d23c01547e50cd533d6cd5914ec167e7
whitnotmax <91440193+whitnotmax@users.noreply.github.com>
2022-01-27 22:28:07 -0500

    style and definitions

--- src/App.svelte
+++ src/App.svelte
@@ -12,26 +12,27 @@
 
 	const letters = [
 		
-		{letter: "d", isActive: true, verb: "devenir" ,   canUseAvoir: false},
-		{letter: "r", isActive: false, verb: "revenir",   canUseAvoir: false},
-		{letter: "m", isActive: false, verb: "mourir",    canUseAvoir: false},
-		{letter: "r", isActive: false, verb: "rester",    canUseAvoir: false},
-		{letter: "s", isActive: false, verb: "sortir",    canUseAvoir: true},
-		{letter: "p", isActive: false, verb: "passer",    canUseAvoir: true},
-		{letter: "v", isActive: false, verb: "venir",     canUseAvoir: false},
-		{letter: "a", isActive: false, verb: "arriver",   canUseAvoir: false}, 
-		{letter: "n", isActive: false, verb: "naître",    canUseAvoir: false}, 
-		{letter: "d", isActive: false, verb: "descendre", canUseAvoir: true}, 
-		{letter: "e", isActive: false, verb: "entrer",    canUseAvoir: true}, 
-		{letter: "r", isActive: false, verb: "rentrer",   canUseAvoir: true}, 
-		{letter: "t", isActive: false, verb: "tomber",    canUseAvoir: false}, 
-		{letter: "r", isActive: false, verb: "retourner", canUseAvoir: true}, 
-		{letter: "a", isActive: false, verb: "aller",     canUseAvoir: false}, 
-		{letter: "m", isActive: false, verb: "monter",    canUseAvoir: true}, 
-		{letter: "p", isActive: false, verb: "partir",    canUseAvoir: false}];
+		{letter: "d", isActive: true, verb: "devenir" ,   canUseAvoir: false , definition: "abc"},
+		{letter: "r", isActive: false, verb: "revenir",   canUseAvoir: false , definition: "abc"},
+		{letter: "m", isActive: false, verb: "mourir",    canUseAvoir: false , definition: "abc"},
+		{letter: "r", isActive: false, verb: "rester",    canUseAvoir: false , definition: "abc"},
+		{letter: "s", isActive: false, verb: "sortir",    canUseAvoir: true  , definition: "abc"},
+		{letter: "p", isActive: false, verb: "passer",    canUseAvoir: true  , definition: "abc"},
+		{letter: "v", isActive: false, verb: "venir",     canUseAvoir: false , definition: "abc"},
+		{letter: "a", isActive: false, verb: "arriver",   canUseAvoir: false , definition: "abc"}, 
+		{letter: "n", isActive: false, verb: "naître",    canUseAvoir: false , definition: "abc"}, 
+		{letter: "d", isActive: false, verb: "descendre", canUseAvoir: true  , definition: "abc"}, 
+		{letter: "e", isActive: false, verb: "entrer",    canUseAvoir: true  , definition: "abc"}, 
+		{letter: "r", isActive: false, verb: "rentrer",   canUseAvoir: true  , definition: "abc"}, 
+		{letter: "t", isActive: false, verb: "tomber",    canUseAvoir: false , definition: "abc"} , 
+		{letter: "r", isActive: false, verb: "retourner", canUseAvoir: true  , definition: "abc"}, 
+		{letter: "a", isActive: false, verb: "aller",     canUseAvoir: false , definition: "abc"}, 
+		{letter: "m", isActive: false, verb: "monter",    canUseAvoir: true  , definition: "abc"}, 
+		{letter: "p", isActive: false, verb: "partir",    canUseAvoir: false , definition: "abc"}];
 	
 	let textbox;
 	let labelText = "Verb:";
+	let definitionText = "";
 	let usingAvoir = false;
 
 	function handleKeyDown(event) {
@@ -47,6 +48,7 @@
 
 				if (textbox.value.toLowerCase() === letters[i].verb.toLowerCase() && usingAvoir === letters[i].canUseAvoir) {
 					labelText = "Verb:";
+					definitionText = "";
 					textbox.value = "";
 					letters[i].isActive = false;
 					usingAvoir = false;
@@ -59,6 +61,7 @@
 				break;
 				} else {
 					labelText = "Incorrect, try again:";
+					definitionText = letters[i].definition;
 				}
 				
 			}
@@ -67,44 +70,75 @@
 	}
 
 </script>
-<div class="container">
-	<span>
-		{#each letters as letter}
-		<Vandertramp letter={letter.letter} isActive={letter.isActive}/>
-		{/each}
 
+<svelte:body on:keydown={handleKeyDown}></svelte:body>
+
+<div class="container centered column">
+	<div class="white curved padded">
+		<span>
+			{#each letters as letter}
+			<Vandertramp letter={letter.letter} isActive={letter.isActive}/>
+			{/each}
+	
 		</span>
-		<label for="verb" class="padded">{labelText}</label>
-		<input type="text" id="verb" name="verb" bind:this={textbox} on:keydown="{handleKeyDown}">
-		<div>
-			Does it use Avoir?
-			<label>
-				<input type=radio bind:group={usingAvoir} name="yes" value={true}>
-				Yes
-			</label>
-			<label>
-				<input type=radio bind:group={usingAvoir} name="yes" value={false}>
-				No
-			</label>
-		</div>
-		<div>
-			<button class="padded" on:click={cycle}>Check</button>
-		</div>
+
+			<label for="verb">{labelText}</label>
+			<input type="text" class="curved" id="verb" name="verb" bind:this={textbox}>
+			<div>
+				Does it use Avoir?
+				<label>
+					<input type=radio bind:group={usingAvoir} name="yes" value={true}>
+					Yes
+				</label>
+				<label>
+					<input type=radio bind:group={usingAvoir} name="yes" value={false}>
+					No
+				</label>
+			</div>
+			<span class="centered column">
+				<button class="green curved small-gap" on:click={cycle}>Check</button>
+				<p class="small-gap">{definitionText}</p>
+			</span>
+	</div>
 </div>
 
 <style>
-
+	:global(body) {
+		background-color: lightgray;
+	}
 	.container {
-		height: 100%;
+		height: 100%;	
+	}
+
+	.small-gap {
+		margin: 2px;
+	}
+
+	.centered {
 		display: flex;
-		flex-direction: column;		
+		
 		align-items: center;
 		justify-content: center;
-	
 	}
+
+	.column {
+		flex-direction: column;		
+	}
+
+	.white {
+		background-color: rgba(255, 255, 255, 0.863);
+	}
+
+	.green {
+		background-color: rgb(31, 153, 0);
+	}
+
+	.curved {
+		border-radius: 5px;
+	}
+
 	 .padded {
-		 margin-top: 10px;
-		 margin-bottom: 10px;
+		 padding: 20px;
 	 }
 
 	 label, input, div {
